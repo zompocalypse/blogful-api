4ff0836b91338b1a29f3f8c3c13ef253f322a290
Jason Stankevich <jason.stankevich@gmail.com>
2020-09-23 17:13:39 -0400

    completed

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const ArticleService = require('./articles-service');
 
 const app = express();
 
@@ -14,6 +15,29 @@ const morganOption = (NODE_ENV === 'production')
 app.use(cors());
 app.use(morgan(morganOption));
 app.use(helmet());
+
+app.get('/articles', (req, res, next) => {
+  const knexInstance = req.app.get('db');
+  ArticleService.getAllArticles(knexInstance)
+    .then(articles => {
+      res.json(articles);
+    })
+    .catch(next);
+});
+
+app.get('/articles/:article_id', (req, res, next) => {
+  const knexInstance = req.app.get('db');
+  ArticleService.getById(knexInstance, req.params.article_id)
+    .then(article => {
+      if (!article) {
+        return res.status(404).json({
+          error: { message: `Article doesn't exist` }
+        });
+      }
+      res.json(article);
+    })
+    .catch(next);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
+const knex = require('knex');
 const app = require('./app');
-const { PORT, NODE_ENV } = require('./config');
+const { PORT, DB_URL, NODE_ENV } = require('./config');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL
+});
+
+app.set('db', db);
 
 app.listen(PORT, () => console.log(
   `Server running in ${NODE_ENV} mode on ${PORT}`
